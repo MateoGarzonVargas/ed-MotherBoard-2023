ecf22752dc94c820d913203a19035a6a1715b218
MateoGarzonVargas <148572220+MateoGarzonVargas@users.noreply.github.com>
2023-11-12 01:45:52 +0100

    Creada estructura de clases del proyecto Automóviles.

---
+++ src/Cliente.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+public class Cliente {
+
+        private String dni;
+        private String nombre;
+
+        private String apellidos;
+
+        private String direccion;
+
+    private ArrayList<Venta> ventas;
+
+    private ArrayList<Vehiculo> vehiculos;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public ArrayList<Venta> getVentas() {
+        return ventas;
+    }
+
+    public void setVentas(ArrayList<Venta> ventas) {
+        this.ventas = ventas;
+    }
+
+    public ArrayList<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(ArrayList<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+}
---
+++ src/Venta.java
@@ -0,0 +1,63 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+public class Venta {
+
+    private String codigo;
+    private String fecha;
+
+    private String formaPago;
+
+    private BigDecimal precio;
+
+    private ArrayList<Cliente> clientes;
+
+    private ArrayList<Vehiculo> vehiculos;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(ArrayList<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public ArrayList<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(ArrayList<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+}
